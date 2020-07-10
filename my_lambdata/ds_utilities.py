66517007c7b563c8dc427f44c8a8e8c5900ffac8
bruno-janota <bruno.janota1@gmail.com>
2020-07-09 23:03:19 -0400

    Before running autopep8

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -7,7 +7,8 @@ def enlarge(n):
     ''' This function will multiple the input by 100 '''
     return n * 1000
 
-def train_validation_test_split(X, y, train_size=0.7, val_size=0.1, test_size=0.2, random_state=None, shuffle=True):
+def train_validation_test_split(X, y, train_size=0.7, val_size=0.1, 
+                                test_size=0.2, random_state=None, shuffle=True):
             
     X_train_val, X_test, y_train_val, y_test = train_test_split(
         X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
@@ -25,7 +26,7 @@ if __name__ == '__main__':
     raw_data = load_wine()
     df = pd.DataFrame(data=raw_data['data'], columns=raw_data['feature_names'])
     df['target'] = raw_data['target']
-    breakpoint()
+    # breakpoint()
     
     print(df.shape)
     
